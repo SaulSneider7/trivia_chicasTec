ee8a3f5c7e287d7062c6d4dc364f5b3d50d59990
SaulSneider7 <saulsneider@hotmail.com>
2024-07-16 09:54:00 -0500

    hola mundo

---
+++ js/categorias.js
@@ -0,0 +1,23 @@
+
+
+function pintarNombre() {
+    var jugador = document.getElementById('jugador');
+    
+    jugador.innerHTML = localStorage.getItem("name"); 
+}
+
+pintarNombre();
+
+
+
+
+
+
+
+
+
+// pintarNombre()
+
+// const temas = [
+//     "peliculas", "series", "novelas"
+// ]
